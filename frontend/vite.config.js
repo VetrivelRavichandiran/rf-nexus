0699f7844ad449e0449fb8dc9fd8741f48a164fe
R Vetrivel <vetrivelrp2005@gmail.com>
2026-08-13 18:26:53 +0530

    Add files via upload

---
+++ frontend/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import base44 from "@base44/vite-plugin";
+
+// https://vite.dev/config/
+export default defineConfig({
+  logLevel: "error", // show only errors
+
+  plugins: [
+    base44({
+      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === "true",
+      hmrNotifier: true,
+      navigationNotifier: true,
+      analyticsTracker: true,
+      visualEditAgent: true,
+    }),
+    react(),
+  ],
+
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
